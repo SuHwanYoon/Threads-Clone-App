151f21d62edc1a02f0673c10cecb6fe4ee01ed9b
SuHwanYoon <suhwan6@gmail.com>
2025-08-18 14:53:07 +0900

    modify login view image

--- ThreadsClone/Core/Authentication/Views/LoginView.swift
+++ ThreadsClone/Core/Authentication/Views/LoginView.swift
@@ -27,7 +27,7 @@ struct LoginView: View {
             VStack {
                 Spacer()
 
-                Image("inapp-icon-white2")
+                Image("login-title-image")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 120, height: 120)
